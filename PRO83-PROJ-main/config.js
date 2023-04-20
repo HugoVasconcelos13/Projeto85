0b70856b0aefc4362073dadd3a1fdbfd21d33576
HugoVasconcelos13 <99847502+HugoVasconcelos13@users.noreply.github.com>
2023-04-19 21:05:16 -0300

    Add files via upload

---
+++ PRO83-PROJ-main/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB0WymvQkbM4Cd446OIMPkUw2mxLWgcbUI",
+    authDomain: "espectagrama-fafd6.firebaseapp.com",
+    projectId: "espectagrama-fafd6",
+    storageBucket: "espectagrama-fafd6.appspot.com",
+    messagingSenderId: "166370668208",
+    appId: "1:166370668208:web:8ace6c6c2031a952235575"
+  };
